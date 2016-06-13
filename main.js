d02a970ae04ad2666e48aa40797d1e9fd5c5307d
Ben Teese <ben.teese@gmail.com>
2016-06-13 18:42:20 +1000

    Went to two-space tabs

--- main.js
+++ main.js
@@ -2,16 +2,16 @@ var React = require('react');
 var ReactDOM = require('react-dom');
 
 var adversities = [
-	{description: 'cat died'}, 
-	{description: "didn't get that promotion"}
+  {description: 'cat died'}, 
+  {description: "didn't get that promotion"}
 ];
 
 var Adversities = (props) => (
-	<ul>
-		{props.value.map(adversity => (
-			<li>{adversity.description}</li>
-		))}
-	</ul>
+  <ul>
+  {props.value.map(adversity => (
+    <li>{adversity.description}</li>
+  ))}
+  </ul>
 );
 
 ReactDOM.render(
