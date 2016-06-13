33f854f682a2a329c6ca56de1be4e80a57f99dec
Ben Teese <ben.teese@gmail.com>
2016-06-13 18:26:38 +1000

    Extract Adverisites component

--- main.js
+++ main.js
@@ -1,7 +1,9 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
 
+var Adversities = () => <h1>Hello, world!</h1>;
+
 ReactDOM.render(
-  <h1>Hello, world</h1>,
+  <Adversities/>,
   document.getElementById('example')
 );
