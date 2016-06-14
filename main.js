dd80ccea068d9a0669b8685768016400b727e123
Ben Teese <ben.teese@gmail.com>
2016-06-14 21:19:11 +1000

    Add React Router

--- main.js
+++ main.js
@@ -1,3 +1,6 @@
+var Router = require('react-router').Router
+var Route = require('react-router').Route
+var Link = require('react-router').Link
 var React = require('react');
 var ReactDOM = require('react-dom');
 
