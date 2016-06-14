d8adbca9dd0dfa8cbe3e13a7e1cac457f3548a34
Ben Teese <ben.teese@gmail.com>
2016-06-14 21:50:51 +1000

    Start using webpack-dev-server

---
+++ webpack.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  entry: './main.js',
+
+  output: {
+    filename: 'bundle.js',
+    publicPath: ''
+  },
+
+  module: {
+    loaders: [
+      { test: /\.js$/, exclude: /node_modules/, loader: 'babel-loader?presets[]=es2015&presets[]=react' }
+    ]
+  }
+}
