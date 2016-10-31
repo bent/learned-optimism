7b00e8ff94be193e0bb03f31a4a135c4f53c3558
Ben Teese <ben.teese@gmail.com>
2016-10-31 21:43:25 +1100

    Improved commenting on Match components

--- src/MatchWhenAuthorized.js
+++ src/MatchWhenAuthorized.js
@@ -2,9 +2,11 @@ import React from 'react';
 import { Match, Redirect } from 'react-router';
 import matchPropTypes from './matchPropTypes';
 
+/**
+ * If we have a logged-in user, display the component, otherwise redirect to login page.
+ */
 const MatchWhenAuthorized = ({ component: Component, userRef, ...rest }) => (
   <Match {...rest} render={props => (
-    // If we have a logged-in user, display the component, otherwise redirect to login
     userRef ? <Component userRef={userRef} {...props}/> : <Redirect to={{pathname: '/login'}}/>
   )}/>
 )
--- src/MatchWhenUnauthorized.js
+++ src/MatchWhenUnauthorized.js
@@ -2,9 +2,11 @@ import React from 'react';
 import { Match, Redirect } from 'react-router'; 
 import matchPropTypes from './matchPropTypes';
 
+/**
+ * If we have a logged-in user, redirect to the home page. Otherwise, display the component.
+ */
 const MatchWhenUnauthorized = ({ component: Component, userRef, ...rest }) => (
   <Match {...rest} render={props => (
-    // If we have a logged-in user, redirect to the home page. Otherwise, display the component.
     userRef ? <Redirect to={{pathname: '/'}}/> : <Component userRef={userRef} {...props}/>
   )}/>
 )
