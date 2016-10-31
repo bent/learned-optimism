d05d0dc1e541d537d490b27ee44b2f2abcffbb61
Ben Teese <ben.teese@gmail.com>
2016-10-31 21:26:23 +1100

    Stripped down MatchWhenAuthorized

--- src/MatchWhenAuthorized.js
+++ src/MatchWhenAuthorized.js
@@ -4,15 +4,8 @@ import firebase from 'firebase';
 
 const MatchWhenAuthorized = ({ component: Component, userRef, ...rest }) => (
   <Match {...rest} render={props => (
-    // If we have a logged-in user,
-    userRef ? (
-      <Component userRef={userRef} {...props}/>
-    ) : (
-      <Redirect to={{
-        pathname: '/login',
-        state: { from: props.location }
-      }}/>
-    )
+    // If we have a logged-in user, display the component, otherwise redirect to login
+    userRef ? <Component userRef={userRef} {...props}/> : <Redirect to={{pathname: '/login'}}/>
   )}/>
 )
 
