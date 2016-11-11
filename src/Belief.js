667df003383a95b4c623deff031791269c975c9d
Ben Teese <ben.teese@gmail.com>
2016-11-11 21:15:15 +1100

    Belief userRef and beliefId props must be defined

--- src/Belief.js
+++ src/Belief.js
@@ -15,9 +15,9 @@ export default React.createClass({
   mixins: [ReactFireMixin],
   propTypes: {
     params: React.PropTypes.shape({
-      beliefId: React.PropTypes.string
+      beliefId: React.PropTypes.string.isRequired
     }).isRequired,
-    userRef: React.PropTypes.instanceOf(firebase.database.Reference)
+    userRef: React.PropTypes.instanceOf(firebase.database.Reference).isRequired
   },
   componentWillMount() {
     this._loadData(this.props.userRef, this.props.params.beliefId);
@@ -65,28 +65,26 @@ export default React.createClass({
     );
   },
   _loadData(userRef, beliefId) {
-    if (userRef && beliefId) {
-      const beliefsRef = userRef.child('beliefs');
-      const beliefRef = beliefsRef.child(beliefId);
-      this.bindAsObject(beliefRef, 'belief');
+    const beliefsRef = userRef.child('beliefs');
+    const beliefRef = beliefsRef.child(beliefId);
+    this.bindAsObject(beliefRef, 'belief');
 
-      // Once we've got the belief, load the adversity that it belongs to, and all of its beliefs so
-      // that we can set up links correctly
-      beliefRef.once('value').then(snapshot => {
-        const adversityId = snapshot.val().adversityId;
-        this.bindAsObject(
-          userRef.child('adversities').child(adversityId), 'adversity'
-        );
-        beliefsRef.orderByChild('adversityId').equalTo(adversityId).once('value').then(snapshot => {
-          let beliefs = [];
+    // Once we've got the belief, load the adversity that it belongs to, and all of its beliefs so
+    // that we can set up links correctly
+    beliefRef.once('value').then(snapshot => {
+      const adversityId = snapshot.val().adversityId;
+      this.bindAsObject(
+        userRef.child('adversities').child(adversityId), 'adversity'
+      );
+      beliefsRef.orderByChild('adversityId').equalTo(adversityId).once('value').then(snapshot => {
+        let beliefs = [];
 
-          snapshot.forEach(data => {
-            beliefs = beliefs.concat(Object.assign({'.key': data.key}, data.val()));
-          });
-
-          this.setState({beliefs});
+        snapshot.forEach(data => {
+          beliefs = beliefs.concat(Object.assign({'.key': data.key}, data.val()));
         });
+
+        this.setState({beliefs});
       });
-    }
+    });
   }
 });
