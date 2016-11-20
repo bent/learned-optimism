4928226526422c7df9c9b27f51faa2557e1bad62
Ben Teese <ben.teese@gmail.com>
2016-11-20 14:15:27 +1100

    Simplified

--- src/AdversitiesRoute.js
+++ src/AdversitiesRoute.js
@@ -4,23 +4,21 @@ import { Match } from 'react-router';
 import Adversities from './Adversities';
 import Adversity from './Adversity';
 
-export default props => {
-  return (
-    <div>
-      <Match 
-        exactly
-        pattern="/adversities" 
-        render={() => <Adversities userRef={props.userRef}/>}
-      />
-      <Match 
-        pattern="/adversities/:adversityId" 
-        render={adversityProps => {
-          return (
-            <Adversity userRef={props.userRef} adversityId={adversityProps.params.adversityId}/>
-          )
-        }}
-      />
-    </div>
-  )
-}
+export default props => (
+  <div>
+    <Match 
+      exactly
+      pattern="/adversities" 
+      render={() => <Adversities userRef={props.userRef}/>}
+    />
+    <Match 
+      pattern="/adversities/:adversityId" 
+      render={adversityProps => {
+        return (
+          <Adversity userRef={props.userRef} adversityId={adversityProps.params.adversityId}/>
+        )
+      }}
+    />
+  </div>
+)
  
