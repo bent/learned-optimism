8d12d4853930caf5a564f10aab795e7bb5e04f35
Ben Teese <ben.teese@sportsbet.com.au>
2017-02-12 17:02:34 +1100

    Got pager links going again

--- src/PagerLink.js
+++ src/PagerLink.js
@@ -2,15 +2,13 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 const Component = ({to, previous, text}) => (
-  <Link to={to}>{({onClick}) =>
-    // Ideally we would use Pager.Item from react-bootstrap. However, passing onClick through to it
-    // doesn't seem to work, so we're manually reproducing the markup for it here.
-    <li className={previous ? 'previous' : 'next'}>
-      <a href onClick={onClick}>
-        {previous && <span>&larr;</span>} {text} {!previous && <span>&rarr;</span>}
-      </a>
-    </li>
-  }</Link>
+  // Ideally we would use Pager.Item from react-bootstrap. However, passing onClick through to it
+  // doesn't seem to work, so we're manually reproducing the markup for it here.
+  <li className={previous ? 'previous' : 'next'}>
+    <Link to={to}>
+      {previous && <span>&larr;</span>} {text} {!previous && <span>&rarr;</span>}
+    </Link>
+  </li>
 );
 
 Component.propTypes = {
