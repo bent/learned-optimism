6e473568e0f6db89efa7bdfd95dff7b8e4007fe6
Ben Teese <ben.teese@sportsbet.com.au>
2017-03-24 22:16:23 +1100

    Separate out Evidence presentation from container

--- src/Evidence.js
+++ src/Evidence.js
@@ -15,6 +15,55 @@ import List from "./List";
 import disputationPropTypes from "./disputationPropTypes";
 import PagerLink from "./PagerLink";
 
+function Presentation(props) {
+  const { belief, beliefs } = props;
+  const beliefId = belief[".key"];
+  const index = beliefs.findIndex(b => b[".key"] === beliefId);
+  if (index < 0) throw new Error(`Belief with ID ${beliefId} not found`);
+
+  let previousText = "Beliefs";
+  let previousPath = `/adversities/${belief.adversityId}`;
+
+  if (index > 0) {
+    previousText = "Prev. Belief";
+    previousPath = `/beliefs/${beliefs[index - 1][".key"]}/alternatives`;
+  }
+
+  return (
+    <div>
+      <Form onSubmit={props.handleSubmit}>
+        <ControlLabel>
+          What evidence is there that&nbsp;
+          {lowerCaseFirstLetter(belief.description)}?
+        </ControlLabel>
+        <FormGroup>
+          <InputGroup>
+            <FormControl
+              type="text"
+              placeholder="Evidence"
+              value={props.description}
+              onChange={props.handleChange}
+            />
+            <InputGroup.Button>
+              <Button type="submit" disabled={props.isSaving}>
+                Add
+              </Button>
+            </InputGroup.Button>
+          </InputGroup>
+        </FormGroup>
+      </Form>
+      <List value={props.evidence} />
+      <Pager>
+        <PagerLink to={previousPath} previous text={previousText} />
+        <PagerLink
+          to={`/beliefs/${beliefId}/alternatives`}
+          text="Alternatives"
+        />
+      </Pager>
+    </div>
+  );
+}
+
 export default React.createClass({
   propTypes: disputationPropTypes,
   mixins: [ReactFireMixin],
@@ -27,52 +76,17 @@ export default React.createClass({
     this.bindAsArray(this.props.beliefRef.child("evidence"), "evidence");
   },
   render() {
-    const belief = this.props.belief;
-    const beliefs = this.props.beliefs;
-    const beliefId = belief[".key"];
-    const index = beliefs.findIndex(b => b[".key"] === beliefId);
-    if (index < 0) throw new Error(`Belief with ID ${beliefId} not found`);
-
-    let previousText = "Beliefs";
-    let previousPath = `/adversities/${belief.adversityId}`;
-
-    if (index > 0) {
-      previousText = "Prev. Belief";
-      previousPath = `/beliefs/${beliefs[index - 1][".key"]}/alternatives`;
-    }
-
+    const { state, props } = this;
     return (
-      <div>
-        <Form onSubmit={this.handleSubmit}>
-          <ControlLabel>
-            What evidence is there that&nbsp;
-            {lowerCaseFirstLetter(belief.description)}?
-          </ControlLabel>
-          <FormGroup>
-            <InputGroup>
-              <FormControl
-                type="text"
-                placeholder="Evidence"
-                value={this.state.description}
-                onChange={this.handleChange}
-              />
-              <InputGroup.Button>
-                <Button type="submit" disabled={this.state.isSaving}>
-                  Add
-                </Button>
-              </InputGroup.Button>
-            </InputGroup>
-          </FormGroup>
-        </Form>
-        <List value={this.state.evidence} />
-        <Pager>
-          <PagerLink to={previousPath} previous text={previousText} />
-          <PagerLink
-            to={`/beliefs/${beliefId}/alternatives`}
-            text="Alternatives"
-          />
-        </Pager>
-      </div>
+      <Presentation
+        belief={props.belief}
+        beliefs={props.beliefs}
+        handleSubmit={this.handleSubmit}
+        description={state.description}
+        handleChange={this.handleChange}
+        isSaving={state.isSaving}
+        evidence={state.evidence}
+      />
     );
   },
   handleChange(e) {
