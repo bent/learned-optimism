2db73d2c101d19e3d255ef38c7d5b1a077be6cc2
Ben Teese <ben.teese@sportsbet.com.au>
2017-04-10 22:33:29 +1000

    Got basic confirmation button going

--- src/Remove.js
+++ src/Remove.js
@@ -1,14 +1,29 @@
 import React from "react";
+import { Button } from "react-bootstrap";
 
-const Component = props => (
-  <span
-    onClick={e => {
-      e.preventDefault();
-      props.remove();
-    }}
-    className="remove glyphicon glyphicon-remove"
-  />
-);
+const Component = React.createClass({
+  getInitialState() {
+    return { showConfirmation: false };
+  },
+  render() {
+    return !this.state.showConfirmation
+      ? <span
+          className="remove glyphicon glyphicon-remove"
+          onClick={this._showConfirmation}
+        />
+      : <Button bsStyle="danger" onClick={this._delete}>
+          Delete
+        </Button>;
+  },
+  _showConfirmation(e) {
+    e.preventDefault();
+    this.setState({ showConfirmation: true });
+  },
+  _delete(e) {
+    e.preventDefault();
+    this.props.remove();
+  }
+});
 
 Component.propTypes = {
   // Callback to be invoked if removal is to occur
