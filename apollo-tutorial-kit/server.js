24c5674bc81b0f227ea233136ca52b37660626dd
Ben Teese <ben.teese@sportsbet.com.au>
2018-01-03 15:26:08 +1100

    Got CORS configured

--- apollo-tutorial-kit/server.js
+++ apollo-tutorial-kit/server.js
@@ -1,12 +1,14 @@
 import express from 'express';
 import { graphqlExpress, graphiqlExpress } from 'apollo-server-express';
 import bodyParser from 'body-parser';
+import cors from 'cors'
 import schema from './data/schema';
 
 const GRAPHQL_PORT = 4000;
 
 const graphQLServer = express();
 
+graphQLServer.use(cors())
 graphQLServer.use('/graphql', bodyParser.json(), graphqlExpress({ schema }));
 graphQLServer.use('/graphiql', graphiqlExpress({ endpointURL: '/graphql' }));
 
