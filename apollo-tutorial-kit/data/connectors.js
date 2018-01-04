18497202891eec93908be0c3da1285192a4144df
Ben Teese <ben.teese@sportsbet.com.au>
2018-01-04 14:24:22 +1100

    Add support for evidence, alternatives, implications

--- apollo-tutorial-kit/data/connectors.js
+++ apollo-tutorial-kit/data/connectors.js
@@ -18,6 +18,27 @@ const BeliefModel = db.define('belief', {
 AdversityModel.hasMany(BeliefModel);
 BeliefModel.belongsTo(AdversityModel);
 
+const EvidenceModel = db.define('evidence', {
+  description: { type: Sequelize.STRING }
+})
+
+BeliefModel.hasMany(EvidenceModel)
+EvidenceModel.belongsTo(BeliefModel)
+
+const AlternativeModel = db.define('alternative', {
+  description: { type: Sequelize.STRING }
+})
+
+BeliefModel.hasMany(AlternativeModel)
+AlternativeModel.belongsTo(BeliefModel)
+
+const ImplicationModel = db.define('implication', {
+  description: { type: Sequelize.STRING }
+})
+
+BeliefModel.hasMany(ImplicationModel)
+ImplicationModel.belongsTo(BeliefModel)
+
 // create mock data with a seed, so we always get the same
 casual.seed(123);
 db.sync({ force: true }).then(() => {
@@ -27,7 +48,11 @@ db.sync({ force: true }).then(() => {
     }).then((adversity) => {
       return adversity.createBelief({
         description: casual.sentences(1),
-      });
+      }).then(belief => Promise.all([
+        belief.createEvidence({description: casual.sentences(1)}),
+        belief.createAlternative({description: casual.sentences(1)}),
+        belief.createImplication({description: casual.sentences(1)})
+      ]))
     });
   });
 });
